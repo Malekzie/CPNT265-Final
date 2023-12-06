56d0ce08fb7eb139cb4f19c7b0b25aa329ab2fc5
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-06 12:27:55 -0700

    Layout set for landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
 	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	// Skeleton UI Imports
+	import { AppShell } from '@skeletonlabs/skeleton'
+
+	// Component Imports
+	import SideBar from '$lib/components/SideBar.svelte';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<slot />
+
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">
+		<SideBar />
+	</svelte:fragment>
+
+	<svelte:fragment slot="pageHeader">
+		<Header/>
+	</svelte:fragment>
+	
+	<slot />
+</AppShell>
