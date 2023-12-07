6d7cff453633766fde2d6b018417d9a72c26f45a
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-07 11:21:22 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,6 @@
 <script>
 	import Hero from '$lib/components/Hero.svelte';
-	import { browser } from '$app/environment';
 
-	if (browser) {
-    const button = document.querySelector('.button');
-    const p = document.querySelector('.fucker');
-    p.innerHTML="work pls"
-    console.log(p)
-	}
 </script>
 
 <Hero />
-
-<button class="button">Sup</button>
-
-<p class="fucker">a</p>
