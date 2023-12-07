f920a03678c0b7cdef21ff692e525d5b445e8efa
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-07 11:21:01 -0700

    Componentized sections

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+
+<div class="flex justify-between m-5">
+  <div class="carouselDetails">
+    <h1>Details</h1>
+  </div>
+  <div class="carouselImage">
+    <h1>Sup</h1>
+  </div>
+</div>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,14 +1,47 @@
+<script>
+	// import { browser } from '$app/environment';
+	// // import { onMount } from 'svelte';
+	// // import Modal from './Modal.svelte';
+
+	import Carousel from "./Carousel.svelte";
+
+	// // onMount(() => {
+	// // 	if (browser) {
+	// // 		let modal = document.getElementById('modal');
+
+	// // 		const openModal = function () {
+	// // 			modal.showModal();
+	// // 		};
+	// // 		setTimeout(openModal, 1000);
+	// // 	}
+	// // });
+
+
+
+
+</script>
+
 <section>
-  <div class="w-[95%]">
-    <!-- Carousel Hero -->
-    <div class="h-[20rem]  m-5 rounded-tl-lg rounded-bl-lg bg-slate-700">
-      <div>
-    
-      </div>
-    </div>
-    <!-- Top Rated Hero -->
-    <div>
-    
-    </div>
-  </div>
+	<div class="w-[95%] flex">
+		<!-- Carousel Hero -->
+		<div class="h-[20rem] w-[70%] mx-auto m-5 rounded-lg bg-slate-700 flex justify-between">
+			<!-- Larger Container -->
+			<div class="w-[50%] h-[90%] my-4 ml-4 bg-green-500 rounded-tl-lg rounded-bl-lg">
+        <Carousel />
+			</div>
+      
+			<!-- Smaller Container -->
+			<div
+				class="w-[50%] h-[90%] my-4 mr-4 bg-green-500 rounded-tr-lg rounded-br-lg border-l-2 border-solid border-black overflow-auto"
+			>
+				<div class="flex flex-col justify-end" />
+			</div>
+		</div>
+	</div>
+
+	<button />
 </section>
+
+<!-- <dialog id="modal" class="rounded-lg">
+  <Modal />
+</dialog> -->
