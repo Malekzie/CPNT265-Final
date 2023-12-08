fa8a23b110b44da83c6e76ef5752c6ca0d4a03e8
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-08 11:45:29 -0700

    New routes

---
+++ src/routes/account/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+   import { AppShell } from '@skeletonlabs/skeleton'
+	import Navbar from '$lib/components/account-components/Navbar.svelte';
+</script>
+
+<AppShell>
+    <svelte:fragment slot="header">
+        <Navbar />
+    </svelte:fragment>
+    <slot></slot>
+</AppShell>
---
+++ src/routes/account/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let data;
+</script>
+
+<div>
+    <ul>
+    {#each data.test as item}
+        <li>{item.first_name}, {item.last_name}</li>
+        {/each}
+    </ul>
+</div>
