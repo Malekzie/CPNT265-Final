85068f0e08423a6210a20fa2aab7e442872c48cf
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-08 11:48:21 -0700

    Added layout for future styling

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,13 +1,28 @@
 <script>
-
+  export let tags = ['Magic', 'Archmage', 'Adventure', 'Some'];
 </script>
 
-
-<div class="flex justify-between m-5">
-  <div class="carouselDetails">
-    <h1>Details</h1>
-  </div>
-  <div class="carouselImage">
-    <h1>Sup</h1>
-  </div>
+<div class="flex justify-between m-10">
+	<div class="flex flex-col gap-3 ">
+		<div>
+			<h1 class="text-xl">Archmage's Apprentice</h1>
+      <div class="flex gap-2 w-[15rem] overflow-hidden">
+        {#each tags as tag}
+        {#if tag.length > 4}
+        <p>{tag},</p>
+        {:else}
+        <p>...</p>
+        {/if}
+        {/each}
+      </div>
+		</div>
+		<div>
+			<p>Description</p>
+			<p>Status</p>
+			<p>Author:</p>
+		</div>
+	</div>
+	<div>
+		<img src="images/Manhwa.jpg" alt="" class="h-40 w-32 rounded-lg">
+	</div>
 </div>
