5cbe3612f2a74daf55a3a7e241df5d9fd4320400
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-08 11:50:21 -0700

    Reusable component

---
+++ src/lib/components/account-components/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<section>
+    <div class="align-middle border-t border-solid bg-slate-700">
+        <ul class="flex flex-row justify-center gap-10">
+            <a href="/" class="p-5 transition delay-75 border border-solid hover:scale-105">
+                <li>Subscribed</li>
+            </a><a href="/" class="p-5 transition delay-75 border border-solid hover:scale-105">
+                <li>Comments</li>
+            </a><a href="/" class="p-5 transition delay-75 border border-solid hover:scale-105">
+                <li>Creators</li>
+            </a><a href="/" class="p-5 transition delay-75 border border-solid hover:scale-105">
+                <li>Account</li>
+            </a>
+        </ul>
+    </div>
+</section>
