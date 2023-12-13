11fb0741d269c0826a8d9f7dda81e18ad32854a3
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-13 00:18:27 -0700

    Co-authored-by: Reilly Shea-Paston <HazyInk@users.noreply.github.com>

---
+++ src/lib/components/Latest.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<section>
+  <div class="p-5 mx-32 divide-black rounded-lg bg-slate-500">
+    <div class="flex justify-between border-b border-black">
+      <h1 class="p-2 text-2xl">New & Latest</h1>
+      <button class="px-3 mr-3 text-sm rounded-lg variant-filled-primary">See all</button>
+    </div>
+    <div class="flex justify-between">
+
+    </div>
+  </div>
+</section>
